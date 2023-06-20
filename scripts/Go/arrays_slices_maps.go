af69663eaa8526ea54431ab5968530f6ecd39f24
LJRollerII <ljcoolkid@optonline.net>
2023-06-20 19:38:55 -0400

    Learned about slicers

--- scripts/Go/arrays_slices_maps.go
+++ scripts/Go/arrays_slices_maps.go
@@ -29,6 +29,31 @@ func main() {
 	// Slices //
 
 	//Slices --> Lists in Python
+	s1 := make([]string, 5)
+	s2 := make([]int, 5)
+	
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+	// To append slices //
+	s :=  make([]int, 5)
+	fmt.Println(len(s))
+	s = append(s, elems...60) // elems is short for elements. You can pass as many elements as you want. //
+	fmt.Println(len(s))
+
+	fmt.Println(s)
+
+	// TO copy slices//
+	cp1 := make([]int, len(s))
+	copy(cp1, s) // copy cp1 into slice (s) //
+
+	// Slicing Operator//
+	//This is similar to Python//
+
+	fmt.Println(cp1[1:4]) // This means print 1 , 2, 3  (Just like in Python) //
+	// Go cannot uses negative numbers like Python //
+
 
 	// Maps --> Dictionaries in Python
 }
